e0f73a66a7ae05d2a1e132b275f8ef630ff8f5d3
Gibsongf <maccgiiibson@gmail.com>
2022-12-04 21:17:30 -0300

    home page (hard-code) html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 console.log("Everything looks fine!!")
+
+
+function elementsGenerator (type, name=false,selector) {
+    const el = document.createElement(type)
+    if(name != false){
+        if (selector == 'class'){
+            el.classList.add(name)
+        }
+        if (selector == 'id'){
+            el.setAttribute('id',name)
+        }
+    }
+    return el
+}
+function menu (){
+    const content = document.getElementById('content')
+    
+    let y=elementsGenerator('div','upa','class')
+    content.appendChild(y)
+   
+}
+menu()
