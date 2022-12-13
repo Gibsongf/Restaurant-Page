becb92937e8ce0e4fad01105153cb0d70775326c
Gibsongf <maccgiiibson@gmail.com>
2022-12-12 22:08:51 -0300

    home page index fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import HtmlGenerator from '/src/dom-manipulation.js'
 import BtnIconActive from './MenuStyle.js';
 import "./style.css";
+import {HomePage }from './createPages.js'
 
 const menuDom = HtmlGenerator();
 menuDom.nav()
